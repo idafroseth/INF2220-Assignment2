b2fad334b8d7237ad73e58264e16c54539da04b3
idamfro <idamfro@ulrik.uio.no>
2015-10-06 15:26:18 +0200

    getting the loops

--- src/main/java/assignment2_idamfro/Project.java
+++ src/main/java/assignment2_idamfro/Project.java
@@ -14,25 +14,26 @@ import java.util.Queue;
 public class Project {
 	public Project(String projectName){
 		this.inputFilename = projectName;
-		
 		importTasks();
 		initializeDependantTasks();	
-		topologicalSort();
+		topologicalSort(NUM_TASKS);
 		printTasks();
 		printProjectExecution();
 	}
 	
 	HashMap<Integer, Task> projectTasks;
 	int NUM_TASKS;
+	static int loopCount = 0;
 	String inputFilename;
-	
+	LinkedList<Task> queue;
 	
 	
 	//Multiple tasks can be executed simultanously
 	//Delayed t1 when the only edge from t1 is to t3 and t3 also have a dependency to t2 which takes more time than t1
 	//t1 has then positive slack because it do not have to be done before x time units after schedule
 	//Tasks with slack 0 is critical tasks and someone else depend on it
-	public void topologicalSort(){
+	//Complexity is N tasks + E edges/task
+	public boolean topologicalSort(int projectSize){
 		//int time = startTask().stopTime;
 		//findNextTask(0);
 		int counter = 0;
@@ -42,11 +43,11 @@ public class Project {
 				queue.add(task);
 				task.earliestStartTime = 0;
 				task.stopTime = task.timeConsumption;
-				System.out.println(task.id + " is added as start task" );
 			}
 		}
+		Task workingTask = null;
 		while( queue.size() != 0){
-			Task workingTask = queue.getFirst();
+			workingTask = queue.getFirst();
 			queue.remove(workingTask);
 			++counter;
 			for(Task task : workingTask.dependentTasks){
@@ -57,31 +58,78 @@ public class Project {
 				}
 			}
 		}
-		if( counter != NUM_TASKS){
-			System.out.println("THERE IS A LOOP!");
-			System.exit(0);
-		//	throw new CycleFoundException();
-		}
-		System.out.println(counter + " and number of tasks are: " + NUM_TASKS);
+		return !haveLoop(counter, projectSize);
 	}
-
+	public LinkedList<Task> findStartNodes(){
+		LinkedList<Task> queue = new LinkedList<Task>();
+		for(Task task : projectTasks.values()){
+			if(task.indegree == 0){
+				queue.add(task);
+				task.earliestStartTime = 0;
+				task.stopTime = task.timeConsumption;
+			}
+		}
+		return queue;
+	}
+	
+	//Check if there is a loop. If there is a simple loop it would be able to find the loop, 
+	//if the loop is not only between two, but three or more tasks, we are not able to catch where the loop is
+	//Except eliminate what tasks that potentially can be involved in the loop	
+	public boolean haveLoop(int counter, int projectSize){
+		if( counter != projectSize){
+			System.out.println("LOOP - The project is not realisable....");
+			LinkedList<Task> potTask = new LinkedList<Task>();
+			
+			for(Task task : projectTasks.values()){
+				//to minimize the search
+				if(task.indegree != 0 && task.dependentTasks.size() != 0){
+					potTask.add(task);
+				}
+			}
+			
+			System.out.println("Nodes in a loop are: ");
+			
+			while(potTask.size() != 0){
+				Task workTask = potTask.removeFirst();
+				int startId = workTask.id;
+	
+				LinkedList<Task> track = new LinkedList<Task>();
+				
+				if(checkLoop(workTask, startId)){
+					track.add(workTask);
+				}
+			}
+			System.exit(0);
+			return true;
+			
+		}
+		return false;
+	}
+	/**
+	 * Recursive method that will print a loop. It check if an dependentTask have a way to get to the 
+	 * @param workTask
+	 * @param id
+	 * @return
+	 */
+	private boolean checkLoop(Task workTask, int id){
+		boolean result= false;
+		if(workTask.dependentTasks.size() == 0){
+			return false;
+		}
+		for(Task dependentTask : workTask.dependentTasks){
+			if(dependentTask.id == id){
+				System.out.print(id + ", ");
+				result = true;
+				return true;
+	
+			}else{
+				result = checkLoop(dependentTask, id);
+				//return result;
+			}
+		}
+		return result;
+	}
 	public void printProjectExecution(){
-		/**
-		 * Project Execution
-		 *  Time: 0 Starting: x
-		 *  		Starting: x
-		 * 	   Current staff: x
-		 * 
-		 * Print every task
-		 *  - Identity number 
-		 *  - Name
-		 *  - Time needed to finish the task
-		 *  - ManpowerRequired
-		 *  - Earliest starting time
-		 *  - Latest starting time
-		 *  - slack
-		 */
-		
 		//Easiest way has to be to save the items in a map with the <StartTime, ArrayList<Tasks>>
 		HashMap<Integer, ArrayList<Task>> workingSet = new HashMap<Integer, ArrayList<Task>>();
 		HashMap<Integer, ArrayList<Task>> stoppingSet = new HashMap<Integer, ArrayList<Task>>();
@@ -141,6 +189,10 @@ public class Project {
 		System.out.println("********THE PROJECT WILL FINISH IN: " + time +" *********");
 
 	}
+	
+	/**
+	 * Print the project tasks and its attribute
+	 */
 	public void printTasks(){
 		for(Task task : projectTasks.values()){
 			System.out.println("Task Id: " + task.id +"\n"
@@ -149,8 +201,7 @@ public class Project {
 					+ "Manpower requirements: " + task.staff + "\n"
 					+ "Earliest start time: " + task.earliestStartTime + "\n"
 					+ "Latest start time: " + task.latestStartTime + "\n"
-					+ "Slack: " + task.slack +"\n"
-					+ "Sequence numb: " + task.topoNumber);
+					+ "Slack: " + task.slack +"\n");
 //			Denpendency edges ");
 			System.out.println("dependentTasks: ");
 			for(Task dependency : task.dependentTasks){
@@ -166,6 +217,7 @@ public class Project {
 	
 	/**
 	 * Parsing the inputFile and initilaizes the project
+	 * * (Complexity N)
 	 */
 	public void importTasks(){
 		try {		
@@ -192,6 +244,12 @@ public class Project {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Use the task.dependencies to find which tasks are dependant of a task.
+	 * This is done to simplify the topoSort
+	 * (Complexity N^2)
+	 */
 	public void initializeDependantTasks(){
 		for(Task task : projectTasks.values()){
 			for(Integer taskId : task.dependencies){
@@ -202,10 +260,11 @@ public class Project {
 	
 
 	public static void main(String[] args){
-		if(args.length != 1){
-			System.out.println("Wrong number of arguments, use: java assignment2.Project <projectName>.txt");
-			System.exit(0);
-		}
-		Project project = new Project(args[0]);
+//		if(args.length != 1){
+//			System.out.println("Wrong number of arguments, use: java assignment2.Project <projectName>.txt");
+//			System.exit(0);
+//		}
+//		Project project = new Project(args[0]);
+		Project project = new Project("");
 	}
 }
--- src/main/java/assignment2_idamfro/Task.java
+++ src/main/java/assignment2_idamfro/Task.java
@@ -3,16 +3,15 @@ package assignment2_idamfro;
 import java.util.ArrayList;
 
 public class Task {
-	int id, staff, timeConsumption;
+	int id, staff, timeConsumption, earliestStartTime, latestStartTime, slack;
 	String name;
-	int earliestStartTime, latestStartTime;
-	int stopTime;
+	int stopTime; //Is really startTime + timeConsumption
 	//if slack is 0, then this is a critical task
-	int slack;
+	
 	ArrayList<Integer> dependencies;
 	ArrayList<Task> dependentTasks = new ArrayList<Task>();
 	int indegree;
-	int topoNumber;
+	boolean visited = false;
 	
 	public Task(int id, String name, int timeConsumption, int manpower, ArrayList<Integer> dependencies){
 		this.id = id;
@@ -21,14 +20,15 @@ public class Task {
 		this.staff = manpower;
 		this.dependencies = dependencies;
 		setIndegree(dependencies.size());
-		
-		this.earliestStartTime = 1000;
 	}	
 	public void setIndegree(int numDependencies){
 		this.indegree = numDependencies;
 	}
 	public void addDependentTask(Task task){
 		dependentTasks.add(task);
+	}
+	public void removeDependentTask(Task task){
+		dependentTasks.remove(task);
 	}
 
 }
